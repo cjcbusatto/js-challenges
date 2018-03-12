95a65785a3f8f56452ed53dd4504e003e431b9ed
Claudio Busatto <c.busatto@wille-enginering.com>
2018-03-12 19:38:59 +0100

    Simple test for FizzBuzz challenge

--- tests/_fizzBuzz.js
+++ tests/_fizzBuzz.js
@@ -4,9 +4,9 @@ const solutions = require('../src/solutions')
 
 test('FIZZ BUZZ', t => {
     var arguments = {n1: 3, n2: 5, max: 15};
-    var expectedResult = solutions.fizzBuzz(arguments);
-
     var output = fizzBuzz(arguments);
 
+    var expectedResult = solutions.fizzBuzz(arguments);
+    
     return t.deepEqual(output, expectedResult);
 });
